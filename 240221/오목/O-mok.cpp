13c2908060766fea223bd7273ba40a4cb96ae01c
MIn <tasabi0826@gmail.com>
2024-02-21 05:52:49 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240221/오목/O-mok.cpp
+++ 240221/오목/O-mok.cpp
@@ -7,15 +7,15 @@ int main() {
             cin >> board[i][j];
     }
     bool same[4];
-    for(int i=0; i<4; i++) same[i] = 1;
-    for(int i=1; i<=15; i++){
+    for(int i=1; i<=19; i++){
         for(int j=1; j<=15; j++){
             if(board[i][j] == 0) continue;
+            for(int i=0; i<4; i++) same[i] = 1;
             for(int k=1; k<=4; k++){
-                if(board[i][j] != board[i+k][j]) same[0] = 0;
+                if(board[i][j] != board[i+k][j] || i+k>19) same[0] = 0;
                 if(board[i][j] != board[i][j+k]) same[1] = 0;
-                if(board[i][j] != board[i+k][j+k]) same[2] = 0;
-                if(board[i][j] != board[i+k][j-k]) same[3] = 0;
+                if(board[i][j] != board[i+k][j+k] || i+k>19) same[2] = 0;
+                if(board[i][j] != board[i+k][j-k] || i+k>19) same[3] = 0;
             }
             if(same[0]){
                 cout << board[i][j] << '\n'<< i+2 << " " << j;
@@ -33,9 +33,8 @@ int main() {
                 cout << board[i][j] << '\n'<< i+2 << " " << j-2;
                 return 0;
             }
-            for(int i=0; i<4; i++) same[i] = 1;
         }
     }
-    cout <<0;
+    cout << 0;
     return 0;
 }
