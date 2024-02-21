285cfee20aed48ba80bd0d4859deb0e1cb4b688e
MIn <tasabi0826@gmail.com>
2024-02-21 22:11:42 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240221/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240221/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -14,8 +14,13 @@ int main() {
     for(int i=1; i<=n; i++){
         int cnt = 1;
         for(int j=1; j<=n; j++){
-            if(arr[i][j-1] == arr[i][j]) cnt++;
-            if(cnt == m){
+            if(arr[i][j-1] == arr[i][j]){
+                cnt++;
+            }
+            else{
+                cnt = 1;
+            }
+            if(cnt >=m){
                 ans++;
                 break;
             }
@@ -24,8 +29,13 @@ int main() {
     for(int i=1; i<=n; i++){
         int cnt = 1;
         for(int j=1; j<=n; j++){
-            if(arr[j-1][i] == arr[j][i]) cnt++;
-            if(cnt == m){
+            if(arr[j-1][i] == arr[j][i]){
+                cnt++;
+            }
+            else{
+                cnt = 1;
+            }
+            if(cnt >=m){
                 ans++;
                 break;
             }
