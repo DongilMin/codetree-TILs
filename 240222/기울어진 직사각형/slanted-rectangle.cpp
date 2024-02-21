f3880330f05f304206871485afca16b165874e9e
MIn <tasabi0826@gmail.com>
2024-02-22 02:24:27 +0900

    Title: 기울어진 직사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240222/기울어진 직사각형/slanted-rectangle.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n;
+int arr[21][21];
+int dx[4] = {-1,-1,1,1};
+int dy[4] = {1,-1,-1,1};
+bool range(int i, int j){
+    return 0<i && 0<j && i<=n && j<=n;
+}
+int cal(int x, int y, int row, int col){
+    int curX = x;
+    int curY = y;
+    int sum = arr[curX][curY];
+    for(int i=1; i<=row; i++){
+        curX += dx[0];
+        curY += dy[0];
+        if(range(curX, curY)) sum += arr[curX][curY];
+        else return -1;
+    }
+    for(int i=1; i<=col; i++){
+        curX += dx[1];
+        curY += dy[1];
+        if(range(curX, curY)) sum += arr[curX][curY];
+        else return -1;
+    }
+    for(int i=1; i<=row; i++){
+        curX += dx[2];
+        curY += dy[2];
+        if(range(curX, curY)) sum += arr[curX][curY];
+        else return -1;
+    }
+    for(int i=1; i<=col; i++){
+        curX += dx[3];
+        curY += dy[3];
+        if(range(curX, curY)) sum += arr[curX][curY];
+        else return -1;
+    }
+    return sum;
+
+}
+int solve(int row, int col){
+    int val = -1;
+    for(int i=1; i<n; i++){
+        for(int j=1; j<n; j++){
+            val = max(val, cal(row,col,i,j));
+        }
+    }
+    return val;
+}
+int main() {
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++)
+            cin >> arr[i][j];
+    }
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            ans = max(ans, solve(i,j));
+        }
+    }
+    cout << ans;
+    return 0;
+}
