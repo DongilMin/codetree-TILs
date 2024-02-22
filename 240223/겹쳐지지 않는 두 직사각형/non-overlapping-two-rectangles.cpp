84b15441c21b4880aee9bb0536b5ae3d5540b5f7
MIn <tasabi0826@gmail.com>
2024-02-23 01:53:55 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240223/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.cpp
+++ 240223/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.cpp
@@ -7,57 +7,21 @@ int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
 
 bool in_range(int i,int j){
-    return i>=0 && i<n && j>=0 && j<m;
+    return i<n && j<m;
 }
 
-int solve(int x, int y, int row, int col){
-    int sum = arr[x][y];
-    int cx = row;
-    int cy = col;
-
-    for(int i=1; i<row; i++){
-        cx += dx[0];
-        cy += dy[0];
-        if(in_range(cx,cy)){
-            sum+=arr[cx][cy];
-        }else{
-            return -10000;
-        }
+int getSum(int x, int y, int w, int h){
+    int curr = 0;
+    for(int i=x; i<=x+h; i++){
+        for(int j=y; j<=y+w; j++)
+            curr += arr[i][j];
     }
-
-    for(int i=1; i<col; i++){
-        cx += dx[1];
-        cy += dy[1];
-        if(in_range(cx,cy)){
-            sum+=arr[cx][cy];
-        }else{
-            return -10000;
-        }
-    }
-
-    for(int i=1; i<row; i++){
-        cx += dx[2];
-        cy += dy[2];
-        if(in_range(cx,cy)){
-            sum+=arr[cx][cy];
-        }else{
-            return -10000;
-        }
-    }
-
-    for(int i=1; i<row; i++){
-        cx += dx[3];
-        cy += dy[3];
-        if(in_range(cx,cy)){
-            sum+=arr[cx][cy];
-        }else{
-            return -10000;
-        }
-    }
-    return sum;
+    return curr;
 }
-
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     cin >> n >> m;
 
     for(int i=0; i<n; i++){
@@ -69,18 +33,19 @@ int main() {
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
 
-            for(int k=1; k<n; k++){
-                for(int l=1; l<m; l++){
-
-                    int sum = solve(i,j,k,l);
+            for(int k=0; k<m; k++){
+                for(int l=0; l<n; l++){
+                    if(!in_range(i+l, j+k)) continue;
 
                     for(int a=0; a<n; a++){
                         for(int b=0; b<m; b++){ // 0,0,2,3 / 1,3
-                            if(abs(a-i) < k && (b-j) < l) continue;
 
-                            for(int c=1; c<n; c++){
-                                for(int d=1; d<n; d++){
-                                    ans = max(ans, sum+solve(a,b,c,d));
+                            for(int c=0; c<m; c++){
+                                for(int d=0; d<n; d++){
+                                    if(!in_range(a+d, b+c)) continue;
+                                    if(i+l < a  || j+k < b){
+                                        ans = max(ans, getSum(i,j,k,l) + getSum(a,b,c,d));
+                                    }
                                 }
                             }
                         }
@@ -89,7 +54,7 @@ int main() {
                 }
             }
         }
-    }
+    }// 6 5 3 -4 6 1 + 20
     cout << ans;
     return 0;
 }
