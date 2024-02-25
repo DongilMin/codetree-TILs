4dc20713d6199b79920c366464bc8f5bd94ed292
MIn <tasabi0826@gmail.com>
2024-02-26 08:33:26 +0900

    Title: 1차원 윷놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240226/1차원 윷놀이/yutnori-1d.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n,m,k;
+vector<int>v;
+vector<int>player;
+int score = 0;
+
+bool impossible(int i){
+    return player[i] >= m;
+}
+
+void fun(int turn, int player_index){
+    if(turn == m+1){
+        int tmp = 0;
+        for(int i=1; i<player.size(); i++){
+            if(player[i] >= m){
+                tmp++;
+            }
+        }
+        score = max(score,tmp);
+        return;
+    }
+    for(int j=1; j<player.size(); j++){
+        if(!impossible(j)) {
+            player[j] += v[turn];
+            fun(turn+1, j);
+            player[j] -= v[turn];
+        }
+    }
+
+}
+int main() {
+    cin >> n >> m >> k;
+    v.push_back(0);
+    player.push_back(0);
+    for(int i=1; i<=k; i++) player.push_back(1);
+
+    for(int i=0; i<n; i++){
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+    fun(0,0);
+    cout << score;
+    return 0;
+}
