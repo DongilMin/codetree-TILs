6bdce9d83d06df30be075caec918e3ee8a7b72e2
MIn <tasabi0826@gmail.com>
2024-02-25 16:59:49 +0900

    Title: 겹치지 않게 선분 고르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240225/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n;
+int result = 0;
+vector<pair<int,int>>v;
+vector<pair<int,int>>ans;
+bool visit[20];
+bool check(int i, int j){
+    return v[i].second <= v[j].first;
+}
+
+bool possible(){
+    for(int i=0; i<ans.size(); i++){
+        for(int j=i+1; j<ans.size(); j++){
+            if(!check(i,j)) return false;
+        }
+    }
+    return true;
+}
+void solve(int index){
+    if(index == n){
+        if(possible()) result = max(result, (int)ans.size());
+        return;
+    }
+
+    ans.push_back(v[index]);
+    solve(index+1);
+    ans.pop_back();
+    solve(index+1);
+}
+
+int main() {
+    int a,b;
+    cin >> n;
+    int tmp = n;
+    while(tmp--){
+        cin >> a >> b;
+        v.push_back(make_pair(a,b));
+    }
+    solve(0);
+    cout << result;
+    return 0;
+}
