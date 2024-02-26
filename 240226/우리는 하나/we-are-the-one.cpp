865c2844b215ee5bfbe6378d7e9e6c1ba47c37a0
MIn <tasabi0826@gmail.com>
2024-02-26 20:25:14 +0900

    Title: 우리는 하나, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240226/우리는 하나/we-are-the-one.cpp
+++ 240226/우리는 하나/we-are-the-one.cpp
@@ -10,8 +10,8 @@ int visited[101][101] = {0};
 int n, u, d;
 int ans = 0;
 
-int dfs(){
-    int cnt = q.size();
+int dfs(int size){
+    int cnt = size;
     while(!q.empty()){
         auto cur = q.front();
         q.pop();
@@ -21,10 +21,19 @@ int dfs(){
             if(cx < 1 || cx > n || cy < 1 || cy > n) continue;
             if(u > abs(arr[cx][cy] - arr[cur.first][cur.second]) ||
                 abs(arr[cx][cy] - arr[cur.first][cur.second]) > d) continue;
-            if(0<visited[cx][cy]) continue;
+            if(0 < visited[cx][cy]) continue;
             visited[cx][cy] = 1;
             cnt++;
+            //cout << cx << " and " << cy << "was pushed\n";
             q.push(make_pair(cx,cy));
+        }
+    }
+
+    if(cnt==12){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                cout << visited[i][j] << " ";
+            }cout << endl;
         }
     }
     return cnt;
@@ -33,25 +42,26 @@ int dfs(){
 void reset(){
     for(int i=1; i<=n; i++)
         for(int j=1; j<=n; j++)
-            if(visited[i][j] == 1) visited[i][j] = 0;
-}
-void func(int a,int b, int k){
-    if(k==0){
-        ans = max(ans, dfs());
-        reset();
-        return;
-    }
-    for(int i=a; i<=n; i++){
-        for(int j=b; j<=n; j++){
-            if(0 < visited[i][j]) continue;
-            visited[i][j] = 2;
-            q.push(make_pair(i,j));
-            cout <<  i << " and " << j << " was pushed\n";
-            func(i,j,k-1);
             visited[i][j] = 0;
-        }
-    }
 }
+
+ void func(int a,int b, int k){
+     if(k==0){
+         ans = max(ans, dfs(q.size()));
+         reset();
+         return;
+     }
+     for(int i=a; i<=n; i++){
+         for(int j=b; j<=n; j++){
+             if(visited[i][j] == 0){
+                visited[i][j] = 2;
+                q.push(make_pair(i,j));
+             }
+             func(i,j,k-1);
+             visited[i][j] = 0;
+         }
+     }
+ }
 int main() {
     int k;
     cin >> n >> k >> u >> d;
