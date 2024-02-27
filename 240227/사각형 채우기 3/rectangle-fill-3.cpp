9a914bd1024d3a724f99ef90063391c83bf35c88
MIn <tasabi0826@gmail.com>
2024-02-27 20:51:49 +0900

    Title: 사각형 채우기 3, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240227/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240227/사각형 채우기 3/rectangle-fill-3.cpp
@@ -4,11 +4,11 @@ int main() {
     int n;
     int arr[1001];
     cin >> n;
+    arr[0] = 1;
     arr[1] = 2;
     arr[2] = 7;
-    arr[3] = 22;
-    for(int i=4; i<=n; i++){
-        arr[i] = ((arr[i-2]+3)%1000000007 + (arr[i-1]+2)%1000000007)%1000000007;
+    for(int i=3; i<=n; i++){
+        arr[i] = (arr[i-1]*3; +arr[i-2] -arr[i-3])%1000000007
     }
     // arr[3] = 14+ 8
     // 2 7 22
