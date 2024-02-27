b61b94171dd9b0179987eec9cb0cfbbd389132aa
MIn <tasabi0826@gmail.com>
2024-02-27 19:58:08 +0900

    Title: 계단 오르기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240227/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[1001];
+    int n;
+    cin >> n;
+    arr[0] = 0;
+    arr[1] = 0;
+    arr[2] = 1;
+    arr[3] = 1;
+    for(int i=4; i<=n; i++){
+        arr[i] = arr[i-2] + arr[i-3];
+    }
+    cout << arr[n];
+    return 0;
+}
