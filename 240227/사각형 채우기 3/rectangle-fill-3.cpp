b148d391a3b3d6c640fcf14c89fd47fbc3a5beca
MIn <tasabi0826@gmail.com>
2024-02-27 21:02:22 +0900

    Title: 사각형 채우기 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240227/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240227/사각형 채우기 3/rectangle-fill-3.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+
+#define MAX_N 1000
+#define MOD 1000000007
+
 using namespace std;
+
+// 변수 선언
+int n;
+
+long long dp[MAX_N + 1];
+
 int main() {
-    int n;
-    int arr[1001];
+    // 입력:
     cin >> n;
-    arr[0] = 1;
-    arr[1] = 2;
-    arr[2] = 7;
-    for(int i=3; i<=n; i++){
-        arr[i] = (arr[i-1]*3 +arr[i-2] -arr[i-3])%1000000007;
+
+    // 초기 조건 설정
+    dp[0] = 1;
+    dp[1] = 2;
+
+    // 점화식에 따라 dp값 채우기
+    // dp[i] = dp[i - 1] * 2 + dp[i - 2] * 3 +
+    //         (dp[i - 3] + dp[i - 4] + dp[i - 5] + ... dp[0]) * 2
+    for(int i = 2; i <= n; i++) {
+        dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) % MOD;
+        for(int j = i - 3; j >= 0; j--)
+            dp[i] = (dp[i] + dp[j] * 2) % MOD;
     }
-    cout << arr[n];
+    
+    cout << dp[n];
     return 0;
 }
