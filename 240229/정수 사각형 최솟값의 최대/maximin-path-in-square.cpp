55e445cc7dd98e7d406fcd37c6956ec726ce19ac
MIn <tasabi0826@gmail.com>
2024-02-29 23:07:37 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240229/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
+++ 240229/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -2,29 +2,39 @@
 #include <climits>
 #include <algorithm>
 using namespace std;
-int n;
-int arr[101][101];
-int dp[101][101];
-
 int main() {
     int n;
     cin >> n;
+    int arr[101][101];
+    int dp[101][101];
+    int mindp[101][101];
+    int maxdp[101][101];
+
     for(int i=1; i<=n; i++)
-        for(int j=1; j<=n; j++){
+        for(int j=1; j<=n; j++)
             cin >> arr[i][j];
+
+    mindp[1][1] = maxdp[1][1] = arr[1][1];
+    for(int i=2; i<=n; i++){
+        mindp[1][i] = min(mindp[1][i-1], arr[1][i]);
+        maxdp[1][i] = max(maxdp[1][i-1], arr[1][i]);
+    }
+    for(int i=2; i<=n; i++){
+        mindp[i][1] = min(mindp[i-1][1], arr[i][1]);
+        maxdp[i][1] = max(maxdp[i-1][1], arr[i][1]);
+    }
+    for(int i=2; i<=n; i++){
+        for(int j=2; j<=n; j++){
+            mindp[i][j] = min(max(mindp[i-1][j], mindp[i][j-1]), arr[i][j]);
+            maxdp[i][j] = max(min(maxdp[i-1][j], maxdp[i][j-1]), arr[i][j]);
         }
-    dp[1][1] = arr[1][1];
-    for(int i=2; i<=n; i++)
-        dp[1][i] = min(dp[1][i-1], arr[1][i]);
-
-    for(int i=2; i<=n; i++)
-        dp[i][1] = min(dp[i-1][1], arr[i-1][1]);
-
-    for(int i=2; i<=n; i++)
-        for(int j=2; j<=n; j++)
-            dp[i][j] = min(max(dp[i-1][j], dp[i][j-1]), arr[i][j]);
-
-    
-    cout << dp[n][n];
+    }
+    int ans = INT_MAX;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            dp[i][j] = abs(maxdp[i][j] - mindp[i][j]);
+        }
+    }
+    cout << mindp[n][n];
     return 0;
 }
