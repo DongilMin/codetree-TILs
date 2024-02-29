f5abe43fc3056720e701422f1484e5dda0833b85
MIn <tasabi0826@gmail.com>
2024-02-29 22:20:36 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 164ms, Memory: 5MB, Status: Passed - CodeTree

--- 240229/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 240229/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -1,33 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int dx[4] = {1,-1,0,0};
 int dy[4] = {0,0,-1,1};
+long long arr[501][501];
+
+bool sorting(pair<int,int>a, pair<int,int>b){
+    return arr[a.first][a.second] < arr[b.first][b.second];
+}
+
 int main(){
     int n;
+    vector<pair<int,int>>v;
     int ans = 0;
-    long long arr[501][501];
     int dp[501][501];
     cin >> n;
     for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
             cin >> arr[i][j];
+            v.push_back(make_pair(i,j));
             dp[i][j] = 1;
         }
     }
+    sort(v.begin(), v.end(), sorting);
 
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=n; j++){
-            int record = 0;
-            for(int k=0; k<4; k++){
-                int nx = i + dx[k];
-                int ny = j + dy[k];
-                if(nx<1 || nx > n || ny<1 || ny>n) continue;
-                if(arr[i][j] <= arr[nx][ny]) continue;
-                record = max(record, dp[nx][ny]);
-            }
-            dp[i][j] += record;
-            ans = max(ans, dp[i][j]);
-        }
+    for(int i=0; i<v.size(); i++){
+       int x = v[i].first;
+       int y = v[i].second;
+       for(int j=0; j<4; j++){
+           int nx = x + dx[j];
+           int ny = y + dy[j];
+           if(nx < 1 || nx > n || ny<1 || ny>n) continue;
+           if(arr[x][y] <= arr[nx][ny]) continue;
+           dp[x][y] = max(dp[x][y], dp[nx][ny]+1);
+           ans = max(ans, dp[x][y]);
+       }
     }
     cout << ans;
 }
