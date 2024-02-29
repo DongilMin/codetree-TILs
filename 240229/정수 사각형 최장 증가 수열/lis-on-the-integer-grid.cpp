d8d422644a410de23393b400613605bdc1cfa8e7
MIn <tasabi0826@gmail.com>
2024-02-29 21:47:56 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 70ms, Memory: 2MB, Status: Wrong Answer - CodeTree

--- 240229/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
+++ 240229/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.cpp
@@ -1,50 +1,33 @@
 #include <iostream>
 using namespace std;
-int n;
 int dx[4] = {1,-1,0,0};
 int dy[4] = {0,0,-1,1};
-int arr[501][501];
-int dp[501][501];
-int visited[501][501];
-int ans = 0;
-bool validate(int x, int y){
-
-    for(int i=0; i<4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(nx<1 || nx > n || ny<1 || ny>n) continue;
-        if(arr[x][y] < arr[nx][ny]) return true;
-    }
-    return false;
-}
-void nfind(int x,int y, int cnt){
-    if(!validate(x,y)){
-        ans = max(ans,cnt);
-        return;
-    }
-    for(int i=0; i<4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if(nx<1 || nx >n || ny<1 || ny>n) continue;
-        if(arr[x][y] >= arr[nx][ny]) continue;
-        if(visited[nx][ny]) continue;
-        visited[nx][ny] = true;
-        nfind(nx,ny,cnt+1);
-        visited[nx][ny] = false;
-    }
-    return;
-}
-int main() {
+int main(){
+    int n;
+    int ans = 0;
+    long long arr[501][501];
+    int dp[501][501];
     cin >> n;
-    for(int i=1; i<=n; i++)
-        for(int j=1; j<=n; j++)
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
             cin >> arr[i][j];
+            dp[i][j] = 1;
+        }
+    }
 
     for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
-            nfind(i,j,1);
+            int record = 0;
+            for(int k=0; k<4; k++){
+                int nx = i + dx[k];
+                int ny = j + dy[k];
+                if(nx<1 || nx > n || ny<1 || ny>n) continue;
+                if(arr[i][j] <= arr[nx][ny]) continue;
+                record = max(record, dp[nx][ny]);
+            }
+            dp[i][j] += record;
+            ans = max(ans, dp[i][j]);
         }
     }
     cout << ans;
-    return 0;
 }
