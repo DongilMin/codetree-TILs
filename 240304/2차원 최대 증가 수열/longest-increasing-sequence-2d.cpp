5a7f7281241a5dfc9bad3e3b5a65124e0cc6d2e5
MIn <tasabi0826@gmail.com>
2024-03-04 15:08:41 +0900

    Title: 2차원 최대 증가 수열, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240304/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
+++ 240304/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -9,16 +9,17 @@ int main() {
     for(int i=1; i<=n; i++){
         for(int j=1; j<=m; j++){
             cin >> arr[i][j];
-            dp[i][j] = 1;
+            dp[i][j] = -1;
         }
     }
     int ans = 0;
-
+    dp[1][1] = 1;
     for(int i=1; i<=n; i++){
         for(int j=1; j<=m; j++){
 
             for(int k=1; k<i; k++){
                 for(int l=1; l<j; l++){
+                    if(dp[k][l] == -1)continue;
                     if(arr[k][l] < arr[i][j]) dp[i][j] = max(dp[i][j], dp[k][l]+1);
                 }
             }
