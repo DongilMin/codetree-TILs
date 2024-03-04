7669ba07de6203831cf1d6931e266f57aa2a0e73
MIn <tasabi0826@gmail.com>
2024-03-04 14:08:45 +0900

    Title: 최대 증가 부분 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240304/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240304/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -7,13 +7,14 @@ int main() {
     int dp[1001];
     arr[0] = 0;
     dp[0] = 0;
-    dp[1]= 1;
     cin >> n;
+    
     for(int i=1; i<=n; i++){
         cin >> arr[i];
     }
-
+    dp[1] = 1;
     for(int i=2; i<=n; i++){
+        dp[i] = dp[i-1];
         for(int j=i-1; j>0; j--){
             if(arr[i] > arr[j]){
                 dp[i] = dp[j]+1;
