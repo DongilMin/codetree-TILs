0ec1c17f7282ca77fc8965c417f7e222dff25c93
Min <tasabi0826@gmail.com>
2024-08-28 21:46:03 +0900

    Title: 트로미노, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240828/트로미노/tromino.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int n,m;
+int board[201][201];
+int dx[4] = {-1,1,1,-1};
+int dy[4] = {1,1,-1,-1};
+
+int block(int i,int j){
+    int sum = board[i][j];
+    int add = 0;
+    for(int k=0; k<4; k++){
+        if(i+dx[k]>n || j+dy[k]>m || i+dx[k]<=0 || j+dy[k]<=0) continue;
+        int val = board[i+dx[k]][j] + board[i][j+dy[k]];
+        if(add < val) add = val;
+    }
+    if(i!=1 && i!= n){
+        int row_sum = board[i-1][j] + board[i+1][j];
+        if(row_sum > add) add = row_sum;
+    }
+    if(j!=1 && j!= m){
+        int col_sum = board[i][j-1] + board[i][j+1];
+        if(col_sum > add) add = col_sum;
+    }
+    return sum+add;
+}
+
+int main() {
+    int result = 0;
+
+    cin >> n >> m;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            cin >> board[i][j];
+        }
+    }
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(result < block(i,j)) result = block(i,j);
+        }
+    }
+    cout << result;
+}
