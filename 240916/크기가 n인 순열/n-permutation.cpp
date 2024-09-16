e05cdc90d7eb47661ba251652d51fc68025dcc41
DongilMin <tasabi0826@gmail.com>
2024-09-16 21:25:30 +0900

    Title: 크기가 n인 순열, Time: 43ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/크기가 n인 순열/n-permutation.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n;
+vector<int>v;
+bool visited[10] = {0};
+
+void print(){
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << "\n";
+}
+
+void solve(){
+    if(v.size() == n){
+        print();
+        return;
+    }
+    for(int i=1; i<=n; i++){
+        if(!visited[i]){
+            visited[i] = true;
+            v.push_back(i);
+            solve();
+            v.pop_back();
+            visited[i] = false;
+
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+    solve();
+    return 0;
+}
