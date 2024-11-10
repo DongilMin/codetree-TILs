6529194ae7a67c30d8cfadf82e47d8cdafb27dc7
DongilMin <tasabi0826@gmail.com>
2024-11-10 20:30:21 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 241110/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 241110/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -1,34 +1,55 @@
 #include <iostream>
-#include <queue>
+
+#define MAX_NUM 100
+#define DIR_NUM 2
+
 using namespace std;
-int dx[4] = {0,1,0,-1};
-int dy[4] = {1,0,-1,0};
-queue<pair<int,int>>q;
-int arr[101][101];
-int n,m;
-bool dfs(){
-    while(!q.empty()){
-        auto cur = q.front();
-        if(cur.first == n && cur.second == m) return true;
-        q.pop();
-        for(int i=0; i<2; i++){
-            int cx = cur.first + dx[i];
-            int cy = cur.second + dy[i];
-            if(cx < 1 || cx > n || cy < 1 || cy > m) continue;
-            if(arr[cx][cy] == 0) continue;
-            q.push(make_pair(cx,cy));
-        }
-    }
-    return false;
+
+int n, m;
+int grid[MAX_NUM][MAX_NUM];
+bool visited[MAX_NUM][MAX_NUM];
+
+// 탐색하는 위치가 격자 범위 내에 있는지 여부를 반환합니다.
+bool InRange(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < m;
 }
-int main() {
-    cin >> n >> m;
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=m; j++){
-            cin >> arr[i][j];
+
+// 탐색하는 위치로 움직일 수 있는지 여부를 반환합니다.
+bool CanGo(int x, int y) {
+    if(!InRange(x, y))
+        return false;
+
+    if(visited[x][y] || grid[x][y] == 0)
+        return false;
+
+    return true;
+}
+
+void DFS(int x, int y) {
+    int dx[DIR_NUM] = {0, 1};
+    int dy[DIR_NUM] = {1, 0};
+
+    // 아래쪽과 오른쪽 각각에 대하여 DFS 탐색을 합니다.
+    for(int i = 0; i < DIR_NUM; i++) {
+        int new_x = x + dx[i];
+        int new_y = y + dy[i];
+        if(CanGo(new_x, new_y)) {
+            visited[new_x][new_y] = 1;
+            DFS(new_x, new_y);
         }
     }
-    q.push(make_pair(1,1));
-    cout << dfs() << '\n';
+    
+}
+
+int main() { 
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            cin >> grid[i][j];
+
+    visited[0][0] = 1;
+    DFS(0, 0);
+
+    cout << visited[n - 1][m - 1];
     return 0;
 }
