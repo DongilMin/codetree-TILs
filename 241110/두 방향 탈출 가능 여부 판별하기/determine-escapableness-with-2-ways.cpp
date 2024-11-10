81a26c339f4ff71bfe4dd8221264886a7bd98905
DongilMin <tasabi0826@gmail.com>
2024-11-10 20:29:27 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 466ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241110/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
+queue<pair<int,int>>q;
+int arr[101][101];
+int n,m;
+bool dfs(){
+    while(!q.empty()){
+        auto cur = q.front();
+        if(cur.first == n && cur.second == m) return true;
+        q.pop();
+        for(int i=0; i<2; i++){
+            int cx = cur.first + dx[i];
+            int cy = cur.second + dy[i];
+            if(cx < 1 || cx > n || cy < 1 || cy > m) continue;
+            if(arr[cx][cy] == 0) continue;
+            q.push(make_pair(cx,cy));
+        }
+    }
+    return false;
+}
+int main() {
+    cin >> n >> m;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            cin >> arr[i][j];
+        }
+    }
+    q.push(make_pair(1,1));
+    cout << dfs() << '\n';
+    return 0;
+}
