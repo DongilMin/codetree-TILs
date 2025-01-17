dfa1e3296d71c36df1b2be1c5c419c8d79fd1e3c
DongilMin <tasabi0826@gmail.com>
2025-01-18 01:51:42 +0900

    Title: 행복한 수열의 개수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250118/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 250118/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 int n, m;
@@ -8,6 +7,7 @@ int grid[100][100];
 int main() {
     cin >> n >> m;
 
+    // 입력 받기
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> grid[i][j];
@@ -15,41 +15,47 @@ int main() {
     }
 
     int result = 0;
+
+    // 행 탐색
     for (int i = 0; i < n; i++) {
         int prev = grid[i][0];
-        int cnt = 0;
-        for (int j = 0; j < n; j++) {
-            if(cnt >= m) {
-                result++;
-                break;
-            }
-            if(prev != grid[i][j]){
+        int cnt = 1;
+        bool isHappy = false; // 행복한 수열 여부 확인
+        for (int j = 1; j < n; j++) {
+            if (grid[i][j] != prev) {
                 prev = grid[i][j];
-                cnt = 0;
-            }
-            else{
+                cnt = 1;
+            } else {
                 cnt++;
             }
+            if(cnt>=m){
+                isHappy = true;
+                break;
+            }
         }
+        if (isHappy) result++;
     }
 
+    // 열 탐색
     for (int i = 0; i < n; i++) {
         int prev = grid[0][i];
-        int cnt = 0;
-        for (int j = 0; j < n; j++) {
-            if(cnt >= m) {
-                result++;
-                break;
-            }
-            if(prev != grid[j][i]){
+        int cnt = 1;
+        bool isHappy = false; // 행복한 수열 여부 확인
+        for (int j = 1; j < n; j++) {
+            if (grid[j][i] != prev) {
                 prev = grid[j][i];
-                cnt = 0;
-            }
-            else{
+                cnt = 1;
+            } else {
                 cnt++;
             }
+            if(cnt>=m){
+                isHappy = true;
+                break;
+            }
         }
+        if (isHappy) result++;
     }
 
-    cout << result;
+    cout << result << endl;
+    return 0;
 }
