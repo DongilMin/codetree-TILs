1d517398ea369f08dc75ef30e51887b061cd8fb1
DongilMin <tasabi0826@gmail.com>
2025-03-25 20:36:19 +0900

    Title: 대폭발, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250325/대폭발/big-explosion.cpp
+++ 250325/대폭발/big-explosion.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <queue>
 #include <cmath>
+#include <queue>
 using namespace std;
 
 int n, m, r, c;
@@ -16,28 +16,32 @@ bool in_range(int x, int y) {
 int main() {
     cin >> n >> m >> r >> c;
 
-    vector<pair<int, int>> v;
-    v.push_back({r,c});
-    grid[r][c] = 1;
+    q.push({{r,c},1});
+
     int time = 0;
-    for (int i = 1; i <= m; i++) {
-        for(auto c : v) {
-            int x = c.first;
-            int y = c.second;
-            for (int j = 0; j < 5; j++) {
-                int nx = x + dx[j] * pow(2,i-1);
-                int ny = y + dy[j] * pow(2,i-1);
-                if(!in_range(nx, ny) || grid[nx][ny] == 1) continue;
-                grid[nx][ny] = 1;
-                v.push_back({nx, ny});
-            }
+
+    while(!q.empty()) {
+        auto curr = q.front();
+        q.pop();
+        int t = curr.second;
+        int cx = curr.first.first;
+        int cy = curr.first.second;
+        grid[cx][cy] = 1;
+
+        for (int i = 0; i < 5; i++) {
+            int nx = cx + dx[i] * pow(2, t - 1);
+            int ny = cy + dy[i] * pow(2, t - 1);
+            if(!in_range(nx, ny) || t == m + 1) continue;
+            q.push({{nx,ny}, t + 1});
         }
     }
     int cnt = 0;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            if(grid[i][j] == 1) cnt++;
+             if(grid[i][j] == 1) cnt++;
+            //cout << grid[i][j] << ' ';
         }
+        //cout << '\n';
     }
     cout << cnt << '\n';
     return 0;
