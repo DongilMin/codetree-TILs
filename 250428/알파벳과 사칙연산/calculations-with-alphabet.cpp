92bc78d6b8b65b3e8c44895b3363e9de2c3e8d9e
DongilMin <tasabi0826@gmail.com>
2025-04-28 12:40:40 +0900

    Title: 알파벳과 사칙연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250428/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+string expression;
+int result = 0;
+
+void func(int idx, int len, vector<int>& v){
+    if (idx == len) {
+        int num = v[0];
+        for (int i = 2; i < expression.length(); i += 2) {
+            int val = v[i/2];
+            char op = expression[i - 1];
+            if (op == '-') num -= val;
+            else if (op == '+') num += val;
+            else num *= val;
+        }
+        result = max(result, num);
+        return;
+    }
+
+    for (int i = 1; i <= 4; i++) {
+        v[idx] = i;
+        func(idx + 1, len, v);
+    }
+}
+int main() {
+    cin >> expression;
+    int leng = expression.length();
+    int len = leng % 2 ? leng / 2 + 1 : leng / 2;
+    vector<int> v(len, 1);
+    func(0, len, v);
+    cout << result;
+    return 0;
+}
