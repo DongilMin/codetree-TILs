693cc54254ac3049ba39d42430af16658e71b896
DongilMin <tasabi0826@gmail.com>
2025-04-30 11:51:57 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250430/방향에 맞춰 최대로 움직이기/max-movements-with-direction.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int n;
+int num[5][5];
+int d[5][5];
+int r, c;
+int dir[9][2] = { {0, 0}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1},
+                {1, 0}, {1, -1}, {0, -1}, {-1, -1}}; 
+
+int result = 0;
+void func(int x, int y, int cnt) {
+
+    bool find = 0;
+    for (int i = 1; i < n; i++) {
+        int nx = x + dir[d[x][y]][0] * i;
+        int ny = y + dir[d[x][y]][1] * i;
+
+        if (num[x][y] < num[nx][ny]) {
+            find = 1;
+            func(nx, ny, cnt + 1);
+        }
+    }
+
+    if (!find) {
+        result = max(result, cnt);
+        return;
+    }
+}
+int main() {
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++) cin >> num[i][j];
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++) cin >> d[i][j];
+
+
+    cin >> r >> c;
+    func(r,c,0);
+    cout << result;
+
+    // Please write your code here.
+
+    return 0;
+}
