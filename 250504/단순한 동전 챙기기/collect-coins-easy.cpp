0594641b724db85884c6d49b68d2952de6c75a56
DongilMin <tasabi0826@gmail.com>
2025-05-04 11:26:00 +0900

    Title: 단순한 동전 챙기기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250504/단순한 동전 챙기기/collect-coins-easy.cpp
+++ 250504/단순한 동전 챙기기/collect-coins-easy.cpp
@@ -1,68 +1,91 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <tuple>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <vector>
+
+#define MAX_N 20
+#define COIN_NUM 9
+
 using namespace std;
 
-struct State {
-    int x, y, last, cnt, dist;
-};
+int n;
+int m = 3;
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+char grid[MAX_N][MAX_N];
 
-    int N;
-    cin >> N;
-    vector<string> grid(N);
-    for(int i=0; i<N; i++) 
-        cin >> grid[i];
+vector<pair<int, int> > coin_pos;
+vector<pair<int, int> > selected_pos;
+    
+pair<int, int> start_pos;
+pair<int, int> end_pos;
 
-    int sx=-1, sy=-1;
-    for(int i=0;i<N;i++) for(int j=0;j<N;j++){
-        if(grid[i][j]=='S'){ sx=i; sy=j; }
+int ans = INT_MAX;
+
+int Dist(pair<int, int> a, pair<int, int> b) {
+    int ax, ay;
+    tie(ax, ay) = a;
+    
+    int bx, by;
+    tie(bx, by) = b;
+    
+	return abs(ax - bx) + abs(ay - by);
+}
+
+int Calc() {
+    int num_moves = Dist(start_pos, selected_pos[0]);
+    for(int i = 0; i < m - 1; i++)
+        num_moves += Dist(selected_pos[i], selected_pos[i + 1]);
+    num_moves += Dist(selected_pos[m - 1], end_pos);
+    
+	return num_moves;
+}
+
+void FindMinMoves(int curr_idx, int cnt) {
+    if(cnt == m) {
+        // 선택된 모든 조합에 대해 이동 횟수를 계산합니다.
+        ans = min(ans, Calc());
+        return;
     }
 
-    // visited[x][y][last_num 0..9][collected count 0..3]
-    static bool visited[20][20][10][4] = {};
-    queue<State> q;
-    q.push({sx, sy, 0, 0, 0});
-    visited[sx][sy][0][0] = true;
+    if(curr_idx == (int) coin_pos.size()) 
+        return;
 
-    int dx[4] = {-1,1,0,0}, dy[4] = {0,0,-1,1};
+    // curr_idx index 에 있는 동전을 선택하지 않은 경우
+    FindMinMoves(curr_idx + 1, cnt);
+    
+    // curr_idx index 에 있는 동전을 선택한 경우
+    selected_pos.push_back(coin_pos[curr_idx]);
+    FindMinMoves(curr_idx + 1, cnt + 1);
+    selected_pos.pop_back();
+}
 
-    while(!q.empty()){
-        auto cur = q.front(); q.pop();
-        // 만약 E이고 cnt>=3 이면
-        if(cur.cnt >= 3 && grid[cur.x][cur.y]=='E'){
-            cout << cur.dist << "\n";
-            return 0;
-        }
-        for(int d=0;d<4;d++){
-            int nx = cur.x + dx[d];
-            int ny = cur.y + dy[d];
-            if(nx<0||ny<0||nx>=N||ny>=N) continue;
-
-            char c = grid[nx][ny];
-            int nlast = cur.last;
-            int ncnt  = cur.cnt;
-
-            if(isdigit(c)){
-                int v = c - '0';
-                if(v > cur.last){
-                    // 새 동전 수집
-                    nlast = v;
-                    ncnt  = min(cur.cnt+1, 3); 
-                }
-                // else: 숫자지만 last 이상이면 그냥 지나침
-            }
-            // '.', 'S', 'E' 는 어떤 제약 없이 그냥 지나감
-
-            if(!visited[nx][ny][nlast][ncnt]){
-                visited[nx][ny][nlast][ncnt] = true;
-                q.push({nx, ny, nlast, ncnt, cur.dist+1});
-            }
-        }
-    }
-
-    // 탈출 못 하면
-    cout << -1 << "\n";
-    return 0;
+int main() {
+	cin >> n;
+	
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++) {
+			cin >> grid[i][j];
+			if(grid[i][j] == 'S')
+				start_pos = make_pair(i, j);
+			if(grid[i][j] == 'E')
+				end_pos = make_pair(i, j);
+		}
+    
+    // 동전을 오름차순으로 각 위치를 집어넣습니다.
+    // 이후에 증가하는 순서대로 방문하기 위함입니다.
+    for(int num = 1; num <= COIN_NUM; num++) 
+        for(int i = 0; i < n; i++)
+		    for(int j = 0; j < n; j++)
+                if(grid[i][j] == num + '0')
+                    coin_pos.push_back(make_pair(i, j));
+    
+    FindMinMoves(0, 0);
+	
+	if(ans == INT_MAX)
+		ans = -1;
+    
+	cout << ans;
+	return 0;
 }
