5118363b829a5c9b96b12921509f34824a1ffd17
DongilMin <tasabi0826@gmail.com>
2025-05-12 17:41:07 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 250512/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
+++ 250512/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -11,19 +11,19 @@ int dy[4] = {0, 1, 0, -1};
 bool bfs(int aa, int b) {
     queue<pair<int, int>>q;
     q.push({aa, b});
+    visited[aa][b] = true;
     while(!q.empty()) {
         auto curr = q.front();
         q.pop();
         int x = curr.first;
         int y = curr.second;
-        visited[x][y] = true;
         if(x == n - 1 && y == m - 1) return true;
-
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
             if (a[nx][ny] == 0 || visited[nx][ny]) continue;
+            visited[nx][ny] = true;
             q.push({nx, ny});
         }
     }
