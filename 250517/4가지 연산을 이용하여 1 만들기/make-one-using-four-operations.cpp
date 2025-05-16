d502ce5076b7dd51f2a09f9448c772fa5206f3cc
DongilMin <tasabi0826@gmail.com>
2025-05-17 01:47:53 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250517/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int N;
+int main() {
+    cin >> N;
+
+    int table[1000000];
+    for (int i = 0; i <= N + 1; i++) table[i] = INT_MAX;
+    table[N] = 0;
+    queue<int> q;
+    q.push(N);
+
+    while(!q.empty()) {
+        int curr = q.front();
+        q.pop();
+        if (curr % 2 == 1) {
+            if (table[curr + 1] > table[curr] + 1) {
+                table[curr + 1] = table[curr] + 1;
+                if (curr + 1 > 0) q.push(curr + 1);
+            }
+        }
+        if (curr % 2 == 0) {
+            if (table[curr / 2] > table[curr] + 1) {
+                table[curr / 2] = table[curr] + 1;
+                if (curr / 2 > 0) q.push(curr / 2);
+            }
+        }
+        if (curr % 3 == 0) {
+            if (table[curr / 3] > table[curr] + 1) {
+                table[curr / 3] = table[curr] + 1;
+                if (curr / 3 > 0) q.push(curr / 3);
+            }
+        }
+        if (table[curr - 1] > table[curr] + 1) {
+            table[curr - 1] = table[curr] + 1;
+            if (curr > 1) q.push(curr - 1);
+        }
+    }
+    cout << table[1];
+    return 0;
+}
