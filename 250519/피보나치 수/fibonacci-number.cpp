6b69b40dcfa2d6f2333e8be2bb74cadb355d0446
DongilMin <tasabi0826@gmail.com>
2025-05-19 00:47:26 +0900

    Title: 피보나치 수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250519/피보나치 수/fibonacci-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main() {
+    cin >> N;   
+    int dp[46];
+    dp[0] = 0;
+    dp[1] = 1;
+    for (int i = 2; i <= N; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    cout << dp[N];
+
+    return 0;
+}
