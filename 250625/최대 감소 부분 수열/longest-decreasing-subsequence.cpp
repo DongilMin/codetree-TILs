616219b9e363e338cbf010d8a04a06680e5e86cc
DongilMin <tasabi0826@gmail.com>
2025-06-25 19:56:38 +0900

    Title: 최대 감소 부분 수열, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250625/최대 감소 부분 수열/longest-decreasing-subsequence.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int N;
+int M[1000];
+int dp[1000];
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> M[i];
+        dp[i] = 1;
+    }
+    int result = 1;
+    for (int i = 1; i < N; i++) {
+        for (int j = 0; j < i; j++) {
+            if (M[i] < M[j])
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+        result = max(result, dp[i]);
+    }
+    cout << result;
+    return 0;
+}
