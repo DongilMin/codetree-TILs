c97a990012e6e8e917ca054eebef4802e1d9d295
DongilMin <tasabi0826@gmail.com>
2025-06-26 20:13:56 +0900

    Title: 2차원 최대 증가 수열, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250626/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int grid[50][50];
+int dp[50][50];
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+            dp[i][j] = 1;
+        }
+    }
+
+    int result = -1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+
+            for (int a = i + 1; a < n; a++) {
+                for (int b = j + 1; b < n; b++) {
+                    if (grid[i][j] < grid[a][b]) {
+                        dp[a][b] = max(dp[a][b], dp[i][j] + 1);
+                    }
+                }
+            }
+            // cout << dp[i][j] << ' ';
+            result = max(result, dp[i][j]);
+        }
+        // cout << '\n';
+    }
+
+    cout << result;
+
+    return 0;
+}
