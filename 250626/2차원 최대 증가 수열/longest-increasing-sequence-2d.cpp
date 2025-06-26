ff39fdaf9c8b0511a7e27e4372e27f8fce414be1
DongilMin <tasabi0826@gmail.com>
2025-06-26 20:39:58 +0900

    Title: 2차원 최대 증가 수열, Time: 10ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250626/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
+++ 250626/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 int n, m;
 int grid[50][50];
 int dp[50][50];
+
 int main() {
     cin >> n >> m;
-    for (int i = 0; i < n; i++) {
+
+    for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++) {
             cin >> grid[i][j];
-            dp[i][j] = -1;
+            dp[i][j] = 0;  // 각 칸에서 시작할 수 있다고 가정
         }
-    }
-    dp[0][0] = 0;
-    int result = -1;
+
+    int result = 0;
+
+    // 모든 칸을 출발점으로 생각
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
 
-            for (int a = 0; a < i; a++) {
-                for (int b = 0; b < j; b++) {
-                    if(dp[a][b] == -1) continue;
-                    if (grid[i][j] > grid[a][b]) {
-                        dp[i][j] = max(dp[i][j], dp[a][b] + 1);
+            for (int a = i + 1; a < n; a++) {
+                for (int b = j + 1; b < m; b++) {
+                    if (grid[a][b] > grid[i][j]) {
+                        dp[a][b] = max(dp[a][b], dp[i][j] + 1);
+                        result = max(result, dp[a][b]);
                     }
                 }
             }
-            // cout << dp[i][j] << ' ';
+            // (i,j) 자체에서 시작한 점프 수 고려
             result = max(result, dp[i][j]);
         }
-        // cout << '\n';
     }
 
-    cout << result + 1;
-
+    cout << result + 1 << endl;
     return 0;
 }
