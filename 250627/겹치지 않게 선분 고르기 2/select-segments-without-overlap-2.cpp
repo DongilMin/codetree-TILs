568f9b83f2781beab34dfb535d6cb51382767629
DongilMin <tasabi0826@gmail.com>
2025-06-27 22:31:12 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250627/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
+++ 250627/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
@@ -8,26 +8,27 @@ bool compare(pair<int, int> a, pair<int, int> b) {
 }
 
 int n;
-int x1[1000];
-int x2[1000];
 int dp[1000];
 int main() {
     cin >> n;
     vector<pair<int, int>> v(n);
     for (int i = 0; i < n; i++) {
         cin >> v[i].first >> v[i].second;
+        dp[i] = -1;
     }
 
     sort(v.begin(), v.end(), compare);
 
     dp[0] = 1;
+    int result = 1;
     for (int i = 1; i < n; i++) {
         for (int j = 0; j < i; j++) {
             if (v[j].second < v[i].first) {
                 dp[i] = max(dp[i], dp[j] + 1);
             }
         }
+        result = max(result, dp[i]);
     }
-    cout << dp[n - 1];
+    cout << result;
     return 0;
 }
