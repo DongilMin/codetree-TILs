20ec3e94baa63f0c6fe93070e36a581d5c827e43
DongilMin <tasabi0826@gmail.com>
2025-06-27 23:10:54 +0900

    Title: 알바로 부자 되기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250627/알바로 부자 되기/being-rich-by-working-part-time.cpp
+++ 250627/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -23,7 +23,7 @@ int main() {
     int result = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i; j++) {
-            if (v[j].second < v[i].first)
+            if (v[j].second < v[i].first && v[j].first < v[i].first)
                 dp[i] = max(dp[i], dp[j] + pay[i]);
         }
         result = max(result, dp[i]);
