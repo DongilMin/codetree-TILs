3f69742017af7e4a482ae7e09b0ec92461572b06
DongilMin <tasabi0826@gmail.com>
2025-08-01 21:07:27 +0900

    Title: 뱀은 사과를 좋아해, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250801/뱀은 사과를 좋아해/snake-loves-apples.py
+++ 250801/뱀은 사과를 좋아해/snake-loves-apples.py
@@ -1,12 +1,12 @@
-n, k, l = map(int, input().split())
+n, m, k = map(int, input().split())
 
 grid = [[0] * (n + 1) for _ in range(n + 1)] # 보드
-for _ in range(k):
+for _ in range(m):
     a, b = map(int,input().split()) # 사과의 위치(a, b)
     grid[a][b] = 3                 # 위치에 3넣어줌
 
 info = []
-for _ in range(l):
+for _ in range(k):
     d, p = input().split()   # 방향변환 정보
     info.append((d, int(p))) # 정보를 list에 저장
 
@@ -38,7 +38,7 @@ for mission in info:
         time += 1
         nx = curr_x + directions[direction][0]
         ny = curr_y + directions[direction][1]
-
+        tail = q[len(q) - 1]
         if not in_range(nx, ny):
             print(time)
             exit()
@@ -49,16 +49,16 @@ for mission in info:
             q.insert(0, (nx, ny))
 
         else:
-            tail = q.pop()
-            grid[tail[0]][tail[1]] = 0
-            if grid[nx][ny] == 1:
+            if grid[nx][ny] == 1 and (nx, ny) != tail:
                 print(time)
                 exit()
             else:
+                q.pop()
                 grid[curr_x][curr_y] = 1
                 grid[nx][ny] = 2
                 q.insert(0, (nx, ny))  
         curr_x = nx
         curr_y = ny
 print(time)
+
 
