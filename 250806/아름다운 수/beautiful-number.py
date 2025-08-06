ff6db98e181d8618d43b3bdc9a5e31f5a73dea54
DongilMin <tasabi0826@gmail.com>
2025-08-06 14:41:38 +0900

    Title: 아름다운 수, Time: 196ms, Memory: 24MB, Status: Wrong Answer - Codetree

--- 250806/아름다운 수/beautiful-number.py
+++ 250806/아름다운 수/beautiful-number.py
@@ -10,22 +10,22 @@ def print_output():
 # cnt는 num보다 클 수가 없다.
 # 만약 같은 수가 안나왔는데, cnt != num 이면 False
 # 같은 수가 안나왔을때 cnt == num 이면 PASS
-def is_beautiful():
+def is_beautiful_num():
     # 연달아 같은 숫자가 나오는 시작 위치를 잡습니다.
     i = 0
     while i < n:
         # 만약 연속하여 해당 숫자만큼 나올 수 없다면
         # 아름다운 수가 아닙니다.
-        if i + seq[i] - 1 >= n:
+        if i + selected[i] - 1 >= n:
             return False
         # 연속하여 해당 숫자만큼 같은 숫자가 있는지 확인합니다.
         # 하나라도 다른 숫자가 있다면
         # 아름다운 수가 아닙니다.
-        for j in range(i, i + seq[i]):
-            if seq[j] != seq[i]:
+        for j in range(i, i + selected[i]):
+            if selected[j] != selected[i]:
                 return False
             
-        i += seq[i]
+        i += selected[i]
         
     return True
 
