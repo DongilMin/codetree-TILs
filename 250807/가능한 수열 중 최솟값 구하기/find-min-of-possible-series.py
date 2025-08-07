00fc1f05cc4c2b9945218f418939d01155b0be8e
DongilMin <tasabi0826@gmail.com>
2025-08-07 17:08:53 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 49ms, Memory: 17MB, Status: Passed - Codetree

--- 250807/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 250807/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -1,31 +1,36 @@
 import sys
+sys.setrecursionlimit(10**7)
 
-n = int(input())
-ans = "4"
-selected = []
-ans = []
+# 변수 선언 및 입력
+n = int(sys.stdin.readline())
 nums = [4, 5, 6]
 
-def is_impossible():
-    L = len(selected)
-    for i in range(L):
-        for j in range(1, (L - i)//2 + 1): # j는 길이이다.
-            if selected[i:i + j] == selected[i + j:i + 2*j]:
-                return True
-    return False
+series = []  # 네 original의 selected 리스트
 
-def backtracking(index):
-    global ans
-    if index == n:
-        if not is_impossible():
-            for c in selected:
-                print(c, end="")
-            sys.exit()
-    else:
-        for num in nums:
-            selected.append(num)
-            backtracking(index + 1)
-            selected.pop()
-        return
+# ─── “좋은 수열”인지 검사 ───
+# series 끝에 새로 붙인 한 글자만 검사하기 때문에 O(n)으로 충분히 빠름
+def is_valid_series():
+    L = len(series)
+    for l in range(1, L//2 + 1):
+        # 맨 뒤 l개와 그 앞 l개가 동일하면 안 됨
+        if series[-l:] == series[-2*l:-l]:
+            return False
+    return True
 
+# ─── 백트래킹 ───
+def backtracking(cnt):
+    # 길이 n에 도달하면 바로 출력 후 종료
+    if cnt == n:
+        # 숫자 리스트를 문자열로 바꾸어 붙여 출력
+        print(''.join(map(str, series)))
+        sys.exit(0)
+
+    # 4,5,6 순서대로 시도 → 사전순 최솟값 보장
+    for num in nums:
+        series.append(num)
+        if is_valid_series():       # 여기서 가지치기
+            backtracking(cnt + 1)
+        series.pop()
+
+# 탐색 시작
 backtracking(0)
