bd8d8504fa8fba6f483cf22ef508b04e662db03f
DongilMin <tasabi0826@gmail.com>
2025-08-07 14:38:05 +0900

    Title: 1차원 윷놀이, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250807/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,28 @@
+n, m, k = map(int, input().split())
+m -= 1
+nums = list(map(int, input().split()))
+horses = [0] * k
+ans = 0
+# k개의 말에 대하여 각각이 n개의 경우에 할당된다. O(k*n)
+# 목표지점을 넘어가는 말은 이동 중단
+
+def calc():
+    scores = 0
+    for i in range(k):
+        if horses[i] >= m:
+            scores += 1
+    return scores
+
+def backtracking(curr):
+    global ans
+    if curr == n:
+        ans = max(ans, calc())
+    else:
+        for i in range(k):
+            if horses[i] < m:
+                horses[i] += nums[curr]
+                backtracking(curr + 1)
+                horses[i] -= nums[curr]
+
+backtracking(0)
+print(ans)
