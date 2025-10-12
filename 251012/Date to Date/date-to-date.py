eef6c3559fae442d69fdff24880384655d790f8d
DongilMin <tasabi0826@gmail.com>
2025-10-12 17:29:59 +0900

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251012/Date to Date/date-to-date.py
+++ 251012/Date to Date/date-to-date.py
@@ -3,12 +3,13 @@ m1, d1, m2, d2 = map(int, input().split())
 months = [0, 31, 30, 31, 30 ,31 ,30, 31, 31, 30, 31, 30, 31]
 cnt_day = 0
 while True:
-    d1 += 1
-    cnt_day += 1
 
     if m1 == m2 and d1 == d2:
         print(cnt_day)
         break
+    
+    d1 += 1
+    cnt_day += 1
     
     if months[m1] < d1:
         m1 += 1
