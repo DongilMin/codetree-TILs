1280527f532bc15e4bf928b86f88064f5387c258
DongilMin <tasabi0826@gmail.com>
2025-10-12 17:36:28 +0900

    Title: Date to Date, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251012/Date to Date/date-to-date.py
+++ 251012/Date to Date/date-to-date.py
@@ -1,13 +1,16 @@
 m1, d1, m2, d2 = map(int, input().split())
 
 months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-cnt_day = 0
+
+# 1. 시작일을 포함해야 하므로 1부터 카운트 시작
+cnt_day = 1 
 
 while True:
-
+    # 2. 시작일과 종료일이 같으면 루프를 즉시 종료
     if m1 == m2 and d1 == d2:
-        break 
+        break
     
+    # 3. 날짜를 하루 넘길 때마다 카운트
     cnt_day += 1
     d1 += 1
 
