3f62fd4a41ea1ea19cc71f256da95de711f44679
DongilMin <tasabi0826@gmail.com>
2025-10-13 16:09:41 +0900

    Title: 연속되는 수 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251013/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+result = 0
+cnt = 0
+for i in range(n):
+    if i == 0 or arr[i] != arr[i - 1]:
+        result = max(result, cnt)
+        cnt = 1
+    else:
+        cnt += 1
+
+print(result)
