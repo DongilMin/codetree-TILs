b9f6ca39c4807c9a3657b4c4f177573c79f899d4
DongilMin <tasabi0826@gmail.com>
2025-10-16 14:48:37 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251016/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
+++ 251016/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -1,9 +1,11 @@
+import sys
+
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 x = [p[0] for p in points]
 y = [p[1] for p in points]
 
-ans = 99999
+ans = sys.maxsize
 
 for i in range(n):
     max_x = 1
