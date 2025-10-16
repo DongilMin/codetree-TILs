b567c5ac73ec93e4a5525f01c1de5e310bb1d0dd
DongilMin <tasabi0826@gmail.com>
2025-10-16 20:06:34 +0900

    Title: 삼각형 만들기, Time: 96ms, Memory: 19MB, Status: Passed - Codetree

--- 251016/삼각형 만들기/create-triangle.py
+++ 251016/삼각형 만들기/create-triangle.py
@@ -1,27 +1,32 @@
-import sys
-
+# 변수 선언 및 입력
 n = int(input())
-points = [tuple(map(int, input().split())) for _ in range(n)]
-x = [p[0] for p in points]
-y = [p[1] for p in points]
+points = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-ans = 0
 
+# 삼각형의 넓이에 2를 곱한 값을 반환합니다.
+def area(x1, y1, x2, y2, x3, y3):
+    return abs((x1 * y2 + x2 * y3 + x3 * y1) -
+               (x2 * y1 + x3 * y2 + x1 * y3))
+
+
+# 3개의 점을 모두 골라보면서
+# 조건을 만족하는 경우 중
+# 최대 넓이를 계산합니다.
+max_area = 0
 for i in range(n):
-    max_x = -10001
-    min_x = 10001
-    max_y = -10001
-    min_y = 10001
-    for j in range(n):
-        if j == i:
-            continue
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            # x값이 같은 쌍이 있으며, y값 역시 같은 쌍이 있는 경우에만
+            # 최대 넓이를 계산합니다.
+            x1, y1 = points[i]
+            x2, y2 = points[j]
+            x3, y3 = points[k]
+            if (x1 == x2 or x1 == x3 or x2 == x3) and \
+                (y1 == y2 or y1 == y3 or y2 == y3):
+                max_area = max(max_area, 
+                               area(x1, y1, x2, y2, x3, y3))
 
-        max_x, min_x = max(max_x, x[j]), min(min_x, x[j])
-        max_y, min_y = max(max_y, y[j]), min(min_y, y[j])
-
-        if max_x == min_x or max_y == min_y:
-            continue
-    # print(f"{max_x} and {min_x} && {max_y} and {min_y}")
-    ans = max(ans, abs((max_x - min_x) * (max_y - min_y)))
-
-print(ans)
+print(max_area)
