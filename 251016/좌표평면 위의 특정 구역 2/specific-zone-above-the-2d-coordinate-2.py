34d07f20e56505d1a0d98ef0ad41f69221e87c7b
DongilMin <tasabi0826@gmail.com>
2025-10-16 14:47:05 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251016/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -0,0 +1,24 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+
+ans = 99999
+
+for i in range(n):
+    max_x = 1
+    min_x = 40000
+    max_y = 1
+    min_y = 40000
+    for j in range(n):
+        if j == i:
+            continue
+
+        max_x, min_x = max(max_x, x[j]), min(min_x, x[j])
+        max_y, min_y = max(max_y, y[j]), min(min_y, y[j])
+
+        if max_x == min_x or max_y == min_y:
+            continue
+    ans = min(ans, (max_x - min_x) * (max_y - min_y))
+
+print(ans)
