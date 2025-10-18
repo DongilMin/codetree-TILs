bdc6142a13dce70b95d74f1cbb8a0d1f92ecd9d7
DongilMin <tasabi0826@gmail.com>
2025-10-18 22:47:23 +0900

    Title: 데이터센터의 온도 조정 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251018/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
+++ 251018/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -1,6 +1,7 @@
 N, C, G, H = map(int, input().split())
 ranges = [tuple(map(int, input().split())) for _ in range(N)]
-max_val =
+max_val = max(max(a, b) for a, b in ranges)
+
 def get_score(a, b, t):
     if t < a:
         return C
@@ -8,3 +9,13 @@ def get_score(a, b, t):
         return G
     else:
         return H
+
+ans = 0
+for temp in range(1, max_val + 2):
+    work = 0
+    for a, b in ranges:
+        work += get_score(a, b, temp)
+
+    ans = max(ans, work)
+
+print(ans)
