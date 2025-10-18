b8ac61d7efac61fe293d65f4da57e08519b821c1
DongilMin <tasabi0826@gmail.com>
2025-10-19 02:35:15 +0900

    Title: 등차수열, Time: 63ms, Memory: 17MB, Status: Passed - Codetree

--- 251019/등차수열/arithmetic-sequence.py
+++ 251019/등차수열/arithmetic-sequence.py
@@ -3,16 +3,14 @@ a = list(map(int, input().split()))
 
 a.sort()
 ans = 0
-for k in range(1, n + 1):
-    new_arr = [num for num in a]
-    new_arr.append(k)
-    new_arr.sort()
+for k in range(1, 101):
     cnt = 0
-    for i in range(len(new_arr)):
-        for j in range(i + 1, len(new_arr)):
-            for l in range(j + 1, len(new_arr)):
-                if new_arr[j] - new_arr[i] == new_arr[l] - new_arr[j]:
-                    cnt += 1
+    for i in range(n):
+        for j in range(i + 1, n):
+            d1 = k - a[i]
+            d2 = a[j] - k
+            if d1 != 0 and d2 != 0 and d1 == d2:
+                cnt += 1
     ans = max(ans, cnt)
 
 print(ans)
