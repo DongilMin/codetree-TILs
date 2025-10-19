3cb929a6406a647290f7b426d06a03855bbf9c51
DongilMin <tasabi0826@gmail.com>
2025-10-19 23:15:36 +0900

    Title: 좌표평면 위의 균형 2, Time: 137ms, Memory: 21MB, Status: Wrong Answer - Codetree

---
+++ 251019/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -0,0 +1,30 @@
+n = int(input())
+points = [tuple(map(int, input().split())) for _ in range(n)]
+
+def get_field(x, y, a, b):
+    if y < a and x < b:
+        return 1
+    elif y < a and x > b:
+        return 2
+    elif y > a and x > b:
+        return 3
+    else:
+        return 4
+
+ans = 10**18
+for x in range(1, 101):
+    for y in range(1, 101):
+
+        field = [[] for _ in range(5)]
+        max_val = 0
+        for point in points:
+            a = point[0]
+            b = point[1]
+            field[get_field(x, y, a, b)].append((a, b))
+        
+        max_index = 0
+        for i in range(1, 5):
+            max_index = max(max_index, len(field[i]))
+        ans = min(ans, max_index)
+
+print(ans)
