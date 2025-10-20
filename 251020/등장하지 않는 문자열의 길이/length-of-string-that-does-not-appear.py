35a4f138d76c0d6175137c039bd32ddac93364a6
DongilMin <tasabi0826@gmail.com>
2025-10-20 17:51:22 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251020/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 251020/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -2,11 +2,9 @@ N = int(input())
 s = input()
 
 ans = 10**18
-for i in range(1, N + 1):
+for i in range(2, N + 1):
     char = s[:i]
     if char not in s[i + 1:]:
-        if N == 20:
-            print(char)
         ans = min(ans, i)
 
 print(ans)
