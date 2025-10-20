20e922907ae0bda2cfaa75d8b124b448b0122216
DongilMin <tasabi0826@gmail.com>
2025-10-20 18:00:35 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251020/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 251020/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -1,10 +1,15 @@
-N = int(input())
-s = input()
+N = int(input().strip())
+s = input().strip()
 
-ans = 10**18
-for i in range(2, N + 1):
-    char = s[:i]
-    if char not in s[i:]:
-        ans = min(ans, i)
-
-print(ans)
+for k in range(1, N + 1):
+    seen = set()
+    duplicate = False
+    for i in range(N - k + 1):
+        sub = s[i:i + k]
+        if sub in seen:   # 같은 길이-k 부분문자열이 두 번 이상 등장
+            duplicate = True
+            break
+        seen.add(sub)
+    if not duplicate:
+        print(k)
+        break
